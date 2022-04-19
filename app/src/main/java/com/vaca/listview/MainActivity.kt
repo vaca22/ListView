b5d6ffd995644c871066a390e8d4182954ac95ca
liguanghao <liguanghao@viatomtech.com>
2022-04-19 17:17:27 +0800

    dfgvdfvb

--- app/src/main/java/com/vaca/listview/MainActivity.kt
+++ app/src/main/java/com/vaca/listview/MainActivity.kt
@@ -2,6 +2,8 @@ package com.vaca.listview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +12,21 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val listview=findViewById<ListView>(R.id.fuck)
         listview.adapter=MyAdapter(arrayListOf("fuck1","fuck2"),this)
+        listview.onItemClickListener=object: AdapterView.OnItemClickListener{
+            override fun onItemClick(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+
+            }
+
+        }
+        listview.setSelector(R.color.black)
+//        Thread{
+//            Thread.sleep(1000)
+//            runOnUiThread {
+//                listview.setSelection(0)
+//            }
+//
+//        }.start()
+
+
     }
 }
